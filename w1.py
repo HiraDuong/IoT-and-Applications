3c3842bd15f8a5fa5d4d9a84d6ddd670b6b9ade6
Vũ Huy Hoàng <hirakuminamoto@yahoo.com>
2024-03-29 23:45:06 +0700

    Add files via upload

--- w1.py
+++ w1.py
@@ -1,8 +1,9 @@
 import requests
 import json
+import time
 
-def send_data_urlencoded(api_key, field1, field2):
-    url = f"https://api.thingspeak.com/update?api_key={api_key}&field1={field1}&field2={field2}"
+def send_data_urlencoded(api_key, field1, field2, field3):
+    url = f"https://api.thingspeak.com/update?api_key={api_key}&field1={field1}&field2={field2}&field3={field3}"
     response = requests.get(url)
     return response
 
@@ -19,26 +20,22 @@ def send_data_json(api_key, field1, field2):
     return response
 
 def get_data(api_key, results=2):
-    url = f"https://api.thingspeak.com/channels/1529099/feeds.json?api_key={api_key}&results={results}"
+    url = f"https://api.thingspeak.com/channels/2489794/feeds.json?api_key={api_key}&results={results}"
     response = requests.get(url)
     data = response.json()
     feeds = data['feeds']
     for feed in feeds:
         field1 = feed['field1']
         field2 = feed['field2']
-        print(f"Field1 (Temperature): {field1}, Field2 (Humidity): {field2}")
+        field3 = feed.get('field3', 'N/A')  # Kiểm tra xem trường field3 có tồn tại không
+        print(f"Field1 (Temperature): {field1}, Field2 (Humidity): {field2}, Field3 (Motion): {field3}")
 
 # Thực hiện gửi dữ liệu lên Thingspeak
-api_key = "T7H40F0X82VGW7L5"
-field1 = 20
-field2 = 33
+api_key = "ZVRY8XKDBLU0U245"
 
-response1 = send_data_urlencoded(api_key, field1, field2)
-print("Response from sending data via URL encoded method:", response1.text)
-
-response2 = send_data_json(api_key, field1, field2)
-print("Response from sending data via JSON method:", response2.text)
-
-# Lấy dữ liệu từ Thingspeak API
-print("Data received from Thingspeak API:")
-get_data(api_key)
+# Lặp vô hạn để lấy dữ liệu mỗi 30 giây
+while True:
+    # Lấy dữ liệu từ Thingspeak API
+    print("Data received from Thingspeak API:")
+    get_data(api_key)
+    time.sleep(30)  # Chờ 30 giây trước khi lấy dữ liệu tiếp theo
